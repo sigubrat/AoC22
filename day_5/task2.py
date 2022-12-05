f6f3e48bff8901642546a6a9b9029a19764c9659
Toppris <Sigurdjbratt@hotmail.no>
2022-12-05 11:15:46 +0100

    Solution day 5

---
+++ day_5/task2.py
@@ -0,0 +1,44 @@
+
+
+with open("day_5/data.txt", "r") as f:
+    cargo_lines = []
+    while True:
+        line = f.readline()
+        if line.startswith(' 1'):
+            numbers = [int(i) for i in line.strip().replace(" ", "")]
+            f.readline()
+            break
+        cargo_lines.append(line.strip("\n"))
+
+    print(cargo_lines)
+    cargo = []
+    for i in range(len(numbers)):
+        cargo.append([])
+
+    
+    for line in cargo_lines:
+        i = 0
+        while i < len(line):
+            if line[i] is not " ":
+                crate = line[i:i+3]
+                to = (i/4)
+                cargo[int(to)].append(crate)
+                i += 2
+            i+=1
+    
+    instructions = f.readlines()
+
+    def do_move(num, from_crate, to_crate):
+        for i in range(num, 0, -1):
+            crate = cargo[from_crate-1].pop(i-1)
+            cargo[to_crate-1].insert(0, crate)
+
+
+    for instruction in instructions:
+        instr = instruction.split()
+        do_move(int(instr[1]), int(instr[3]), int(instr[5]))
+    
+    res = "".join([i[0] for i in cargo])
+    print(res)
+
+
